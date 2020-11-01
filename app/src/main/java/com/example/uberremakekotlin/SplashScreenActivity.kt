0bf1035e14909c5f722515ebeb9da15814daafbd
bogos <GKfytnfPtvkz01091987>
2020-11-01 17:57:15 +0200

    Splash Screen

---
+++ app/src/main/java/com/example/uberremakekotlin/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.example.uberremakekotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import io.reactivex.Completable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import java.util.concurrent.TimeUnit
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Completable.timer(3, TimeUnit.SECONDS, AndroidSchedulers.mainThread())
+            .subscribe({
+                Toast.makeText(this@SplashScreenActivity, "splash Screen", Toast.LENGTH_SHORT).show()
+
+            })
+    }
+}
